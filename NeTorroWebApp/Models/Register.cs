900f826c3e684a4389ad21092b15e06a3771de49
Bianca Pantaru <pantarubianca@yahoo.com>
2021-01-06 23:26:35 +0200

    added authorization

---
+++ NeTorroWebApp/Models/Register.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeTorroWebApp.Models
+{
+     public class RegisterModel
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
